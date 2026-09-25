d2a9dda49963f802b612531fb5bd74f8e86bca83
Lars Kellogg-Stedman <lars@redhat.com>
2026-09-25 10:19:12 -0400

    Remove ironic provider from opentofu config
    This required a patched ironic to work and we're not generally making use
    of it.

--- infra/admin/main.tf
+++ infra/admin/main.tf
@@ -5,14 +5,5 @@ terraform {
       source  = "terraform-provider-openstack/openstack"
       version = "~> 1.53.0"
     }
-    ironic = {
-      source = "registry.terraform.io/metal3-community/ironic"
-      version = "~> 1.0.0"
-    }
   }
-}
-
-provider "ironic" {
-  auth_strategy = "keystone"
-  microversion  = "1.72"
 }
